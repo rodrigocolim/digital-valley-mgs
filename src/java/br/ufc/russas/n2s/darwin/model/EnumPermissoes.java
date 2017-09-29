b793f82de9c5bbf22e0a73f87d84d2d9450dd8ca
WallisonCarlos <wallisonrocha2008@gmail.com>
2017-09-29 11:37:09 -0300

    Adição de lib de e-email, upload file, json. Criação da classe usuário, enum permissão

---
+++ src/java/br/ufc/russas/n2s/darwin/model/EnumPermissoes.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufc.russas.n2s.darwin.model;
+
+/**
+ *
+ * @author Wallison Carlos
+ */
+public enum EnumPermissoes {
+    PARTICIPANTE(1),
+    AVALIADOR(2),
+    RESPONSAVEL(3), 
+    ADMINISTRADOR(4);
+    
+    private int nivel;
+    EnumPermissoes(){
+    
+    }
+    
+    EnumPermissoes(int nivel){
+        this.nivel = nivel;
+    }
+    
+    public int getNivel(){
+        return nivel;
+    }
+}

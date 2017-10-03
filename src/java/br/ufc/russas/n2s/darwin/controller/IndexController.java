46f57b6dffc963db91c99fe599d8d35acaf757ea
WallisonCarlos <wallisonrocha2008@gmail.com>
2017-10-03 12:02:58 -0300

    Configuração SpringMVC Annotations Controllers

---
+++ src/java/br/ufc/russas/n2s/darwin/controller/IndexController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufc.russas.n2s.darwin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Wallison Carlos
+ */
+@Controller("indexController")
+@RequestMapping("/")
+public class IndexController {
+    
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String getIndex(){
+        return "redirect:index";
+    }
+}

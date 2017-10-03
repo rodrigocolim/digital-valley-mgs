f7916334661ab4803b29fde0ec81fb2a56ab159a
Lavinia <laviniamatosof@alu.ufc.br>
2017-10-03 09:37:09 -0300

    PeriodoBeans

--- src/java/br/ufc/russas/n2s/darwin/beans/PeriodoBeans.java
+++ src/java/br/ufc/russas/n2s/darwin/beans/PeriodoBeans.java
@@ -5,20 +5,80 @@
  */
 package br.ufc.russas.n2s.darwin.beans;
 
+import br.ufc.russas.n2s.darwin.model.Periodo;
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Lavínia Matoso
  */
 public class PeriodoBeans implements Beans{
 
+    private long codPeriodo;
+    private LocalDateTime inicio;
+    private LocalDateTime termino;
+    
+    public PeriodoBeans(){}
+    public PeriodoBeans(long codPeriodo, LocalDateTime inicio, LocalDateTime termino) {
+        this.codPeriodo = codPeriodo;
+        this.inicio = inicio;
+        this.termino = termino;
+    }
+     
+    public long getCodPeriodo() {
+        return codPeriodo;
+    }
+
+    public void setCodPeriodo(long codPeriodo) {
+        this.codPeriodo = codPeriodo;
+    }
+
+    public LocalDateTime getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(LocalDateTime inicio) {
+        this.inicio = inicio;
+    }
+
+    public LocalDateTime getTermino() {
+        return termino;
+    }
+
+    public void setTermino(LocalDateTime termino) {
+        this.termino = termino;
+    }
+    
     @Override
     public Object toBusiness() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Periodo periodo = new Periodo();
+        
+        if(this.getCodPeriodo() > 0){
+            periodo.setCodPeriodo(this.getCodPeriodo());
+        }
+        periodo.setInicio(this.getInicio());
+        periodo.setTermino(this.getTermino());
+        
+        return periodo;
     }
 
     @Override
     public Beans toBeans(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(object != null){
+            if(object instanceof Periodo){
+                Periodo periodo = (Periodo) object;
+                this.setCodPeriodo(periodo.getCodPeriodo());
+                this.setInicio(periodo.getInicio());
+                this.setTermino(periodo.getTermino());
+                
+                return this;
+            }else{
+                throw new IllegalArgumentException("O objeto a ser adicionado não é um Periodo!");
+            }
+        }else{
+            throw new NullPointerException("Periodo não pode ser nula!");
+        }
+        
     }
     
 }

00b1013b794091d76af3a175f963e64d7d34e4ae
Alex Felipe <alexfelipe@alu.ufc.br>
2017-10-17 09:44:13 -0300

    Up

--- src/java/br/ufc/russas/n2s/darwin/controller/SelecaoController.java
+++ src/java/br/ufc/russas/n2s/darwin/controller/SelecaoController.java
@@ -26,15 +26,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller("SelecaoController")
 @RequestMapping("/")
 public class SelecaoController {
-    ArrayList<SelecaoBeans> novasSelecoes = new ArrayList<>();
+    ArrayList<SelecaoBeans> listaSelecoes = new ArrayList<>();
 
     public SelecaoController() {
         ArrayList<EtapaBeans> etapas = new ArrayList<>();
         etapas.add(new EtapaBeans(2, "Provas", null, "Querias estares in cahsah, mahs estois aquis con fomis y sonis", null, null, null, null, null, false, null));
         etapas.add(new EtapaBeans(3, "Entrevista", null, "Querias estares in cahsah, mahs estois aquis con fomis", null, null, null, null, null, false, null));
         EtapaBeans inscricao = new EtapaBeans(1, "Inscrição", new PeriodoBeans(0, LocalDateTime.now(), LocalDateTime.now()), "Querias estares in cahsah, mahs estois aquis con sedis y fomis y sonis", null, null, null, null, null, false, null);
-        novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));
-    novasSelecoes.add(new SelecaoBeans(2, "Auxilio Emergencial", "A Pró-Reitoria de Assuntos Estudantis da Universidade Federal do Ceará, no uso de suas atribuições administrativas, divulga o resultado do processo seletivo do Auxílio Emergencial – Edital nº 13/2017, referente ao mês de setembro do corrente ano.\n"
+        listaSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));
+        listaSelecoes.add(new SelecaoBeans(2, "Auxilio Emergencial", "A Pró-Reitoria de Assuntos Estudantis da Universidade Federal do Ceará, no uso de suas atribuições administrativas, divulga o resultado do processo seletivo do Auxílio Emergencial – Edital nº 13/2017, referente ao mês de setembro do corrente ano.\n"
             + "\n<br>"
             + "Serão três as situações dos candidatos:\n"
             + "\n<br>"
@@ -49,14 +49,7 @@ public class SelecaoController {
             + "O resultado da análise dos recursos será divulgado no dia 20 de outubro.\n"
             + "\n<br>"
             + "Na mesma data, será publicado o resultado final do processo seletivo do Auxílio Emergencial do Campus Fortaleza, referente ao mês de setembro, o qual indicará os candidatos deferidos, com o respectivo período de concessão, e indeferidos.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));                
-    novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));
-        novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));   
-            novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));   
-                novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));   
-                    novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));   
-                        novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));   
-                            novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));   
-                                novasSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null));   
+        listaSelecoes.add(new SelecaoBeans(1, "Seleção para Bolsa de Iniciação à Docência - 2017.2", "O Diretor do Campus da UFC em Russas no uso de suas atribuições legais e estatutárias, em consonância com a Resolução N° 08/CEPE, de 26 de abril de 2013, torna público o processo de seleção de monitores de iniciação à docência-PID, seguindo as orientações estabelecidas neste Edital e no Edital n° 35/2016 da Pró-Reitoria de Graduação - PROGRAD.", null, inscricao, etapas, 1, 0, "asdasasdas", "asdasdasd", null, null, null, null, null, null)); 
                     
     }
     
@@ -64,14 +57,14 @@ public class SelecaoController {
     @RequestMapping(method = RequestMethod.GET)
     public String getIndex(Model model){
         //DAO<SelecaoBeans> dao = DAO.getInstancia();
-        model.addAttribute("novasSelecoes", novasSelecoes);
+        model.addAttribute("listaSelecoes", listaSelecoes);
         
         return "index";
     }
     
     @RequestMapping(value = "/selecao")
     public String verSelecao(@RequestParam(value = "codSelecao", required = true) int codSelecao, Model model){
-        model.addAttribute("selecao",novasSelecoes.get(codSelecao - 1));
+        model.addAttribute("selecao",listaSelecoes.get(codSelecao - 1));
         return "selecao";
     }
     

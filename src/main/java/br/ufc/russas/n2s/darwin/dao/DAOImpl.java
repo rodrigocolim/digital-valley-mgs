4b9f6a5c98868ee2d82540b4d8100e713b0431a1
WallisonCarlos <wallisonrocha2008@gmail.com>
2017-10-31 10:01:31 -0300

    Correção de erros de acentos

--- src/main/java/br/ufc/russas/n2s/darwin/dao/DAOImpl.java
+++ src/main/java/br/ufc/russas/n2s/darwin/dao/DAOImpl.java
@@ -5,7 +5,6 @@
  */
 package br.ufc.russas.n2s.darwin.dao;
 
-import java.util.Collections;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -13,7 +12,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  *

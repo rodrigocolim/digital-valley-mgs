7527271e98c4701b1f64482a97eac7d872d74846
WallisonCarlos <wallisonrocha2008@gmail.com>
2017-11-09 11:30:23 -0300

    up

--- src/main/java/br/ufc/russas/n2s/darwin/controller/filter/AutenticadoFiltro.java
+++ src/main/java/br/ufc/russas/n2s/darwin/controller/filter/AutenticadoFiltro.java
@@ -1,3 +1,5 @@
+package br.ufc.russas.n2s.darwin.controller.filter;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates

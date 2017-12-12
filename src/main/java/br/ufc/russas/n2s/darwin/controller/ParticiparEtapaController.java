ce47f5c8e23924c1626178cd21a1c6391d6508f1
WallisonCarlos <wallisonrocha2008@gmail.com>
2017-12-12 09:28:43 -0300

    Correção de bugs

--- src/main/java/br/ufc/russas/n2s/darwin/controller/ParticiparEtapaController.java
+++ src/main/java/br/ufc/russas/n2s/darwin/controller/ParticiparEtapaController.java
@@ -25,8 +25,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;

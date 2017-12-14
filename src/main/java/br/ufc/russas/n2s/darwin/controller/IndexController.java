a812aa05a48f16d973f9bf5b65592661e6999a60
Gilberto Lima <betinlimma@gmail.com>
2017-12-14 10:24:15 -0300

    filtro

--- src/main/java/br/ufc/russas/n2s/darwin/controller/IndexController.java
+++ src/main/java/br/ufc/russas/n2s/darwin/controller/IndexController.java
@@ -65,23 +65,6 @@ public class IndexController {
     public String getIndex(Model model, @PathVariable String categoria) {
         Selecao selecao = new Selecao();
         selecao.setCategoria(categoria.replace("_", " "));
-       // EnumEstadoSelecao e = null;
-       // System.out.println("\n\n estado: "+estado +"\n\n");;
-       /* if(estado != null) {
-            if (!estado.toLowerCase().equals("início")) {
-                if (estado.equals("aberta")) {
-                    e = EnumEstadoSelecao.ABERTA;
-                } else if (estado.equals("andamento")) {
-                    e = EnumEstadoSelecao.ANDAMENTO;
-                } else if (estado.equals("finalizada")) {
-                    e = EnumEstadoSelecao.FINALIZADA;
-                }
-                selecao.setEstado(e);
-                model.addAttribute("estado", e.toString().toLowerCase());
-            }
-        }
-        */
-       // System.out.println("\n\n "+ selecao.getEstado().name() +"\n\n");
         List<SelecaoBeans> selecoes = this.getSelecaoServiceIfc().listaSelecoes(selecao);
         HashMap<SelecaoBeans, EtapaBeans> etapasAtuais = new  HashMap<>();
         for (SelecaoBeans s : selecoes) {
@@ -117,9 +100,6 @@ public class IndexController {
         return "index";
     }
     
-    
-
-    
     @RequestMapping(value="/minhas_Selecoes", method = RequestMethod.GET)
 
     public String getMinhasSelecoes(Model model, HttpServletRequest request) {
@@ -127,7 +107,12 @@ public class IndexController {
         UsuarioBeans usuario = (UsuarioBeans) session.getAttribute("usuarioDarwin");
         System.out.println("\n\n");
         List<SelecaoBeans> selecoes = this.getSelecaoServiceIfc().listaSelecoesAssociada(usuario);
+        HashMap<SelecaoBeans, EtapaBeans> etapasAtuais = new  HashMap<>();
+        for (SelecaoBeans s : selecoes) {
+            etapasAtuais.put(s, this.getSelecaoServiceIfc().getEtapaAtual(s));
+        }
         model.addAttribute("selecoes", selecoes);
+        model.addAttribute("etapasAtuais", etapasAtuais);
         return "index";
     }
     

d255217a0b4f214f24f13f4b8bd127ca7f658c5a
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2019-05-07 16:19:42 -0300

    Remoção de alguns arquivos de configuração que não esta sendo utilizados

--- src/test/java/util/TestBean.java
+++ src/test/java/util/TestBean.java
@@ -1,12 +1,5 @@
 package util;
 
-import java.io.File;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import br.ufc.russas.n2s.darwin.beans.ArquivoBeans;
-import br.ufc.russas.n2s.darwin.model.Arquivo;
-
 public class TestBean {
 	public static void main (String args[]) {
 
